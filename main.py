0c8992e0b4deda51263c6d942271e37b3b0359e4
karthikparasa541 <karthik.parasa541@gmail.com>
2026-02-19 21:08:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# saving the api key
+import os
+
+
+#Used langchain, prompt template and chain modules in this code.
+# import ChatAPI Model
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+import streamlit as st
+
+gpt4omini = ChatOpenAI(model_name = "gpt-4o-mini", openai_api_key = st.secrets["OPENAI_API_KEY"])
+
+#We are defining a template below
+query_template = "Generate a {story_type} story with {no_characters} character(s) in {language}"
+
+#preparing the prompt with the template and mentioning the input variables
+query_prompt = PromptTemplate(input_variables = ["story_type","no_characters","language"], template = query_template)
+
+st.header("Story Generator App")
+st.subheader("Generate stories using Generative AI")
+
+story_ty = st.text_input("Enter the type of story you want to generate")
+no_ch = st.number_input("Enter the number of characters in the story",min_value = 1, max_value = 5, value = 1, step = 1)
+lang = st.text_input("Enter the language in which you want the story to be generated")
+
+if st.button("Generate Story"):
+  #invoking the LLM model with the prompt
+  print("***** Begin Story ************")
+  response = gpt4omini.invoke(query_template.format(story_type = story_ty, no_characters = no_ch , language = lang))
+  print(response.content)
+  print("***** End Story ************")
